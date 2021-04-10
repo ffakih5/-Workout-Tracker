505f4058b7d4d4e03b1d3a8d8f2208d2ba1ba9fd
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-04-10 15:01:15 +1000

    create new Workout

--- routes/api.js
+++ routes/api.js
@@ -18,5 +18,33 @@ module.exports = (app) => {
         });
     });
 
+    app.get("/api/workouts/range", (data, res) => {
+        db.Workout.aggregate([
+            {
+                $addFields: {
+                    totalDuration: {
+                        $sum: "$exercises.duration"
+
+                    }
+                }
+            }
+        ])
+            .limit(7)
+            .then(result => {
+                res.json(result)
+            }).catch(err => {
+                res.json(err);
+            })
+    });
+
+    app.post("/api/workouts", (data, res) => {
+        db.Workout.create(data)
+            .then(newWorkout => {
+                res.json(newWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
 
 }

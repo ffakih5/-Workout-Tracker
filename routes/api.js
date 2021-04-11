94916d425405651a885852f2d2dd1db3779ec71b
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-04-11 12:18:24 +1000

    console logging in backend apis to find error crashing heroku

--- routes/api.js
+++ routes/api.js
@@ -12,8 +12,10 @@ module.exports = (app) => {
                 $sort: { day: -1 }
             }
         ]).then(result => {
+            console.log(`<<<<<<<< -------- workouts sending result: ${result} -------- >>>>>>>>`);
             res.json(result)
         }).catch(err => {
+            console.log(`<<<<<<<< -------- workouts failed with error: ${err} -------- >>>>>>>>`);
             res.json(err);
         });
     });
